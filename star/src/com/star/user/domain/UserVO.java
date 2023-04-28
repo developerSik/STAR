f256bf57e83a174ed9489161861ee09ab67f63b4
developerSik <jung9841@gmail.com>
2023-04-28 15:55:58 +0900

    지인님까지

--- star/src/com/star/user/domain/UserVO.java
+++ star/src/com/star/user/domain/UserVO.java
@@ -13,132 +13,139 @@ public class UserVO {
 	private String userTitle;
 	private String userContent;
 	private int userStar;
-	private String userRegistrationDate;
-	private String userEndDate;
+
 	
 	public UserVO() {;}
+
 
 	public Long getUserNumber() {
 		return userNumber;
 	}
 
+
 	public void setUserNumber(Long userNumber) {
 		this.userNumber = userNumber;
 	}
+
 
 	public String getUserId() {
 		return userId;
 	}
 
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 
 	public String getUserPassword() {
 		return userPassword;
 	}
 
+
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
+
 
 	public String getUserName() {
 		return userName;
 	}
 
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 
 	public String getUserAddress() {
 		return userAddress;
 	}
 
+
 	public void setUserAddress(String userAddress) {
 		this.userAddress = userAddress;
 	}
+
 
 	public String getUserPhonenumber() {
 		return userPhonenumber;
 	}
 
+
 	public void setUserPhonenumber(String userPhonenumber) {
 		this.userPhonenumber = userPhonenumber;
 	}
+
 
 	public int getUserAge() {
 		return userAge;
 	}
 
+
 	public void setUserAge(int userAge) {
 		this.userAge = userAge;
 	}
+
 
 	public String getUserGender() {
 		return userGender;
 	}
 
+
 	public void setUserGender(String userGender) {
 		this.userGender = userGender;
 	}
+
 
 	public String getUserStatus() {
 		return userStatus;
 	}
 
+
 	public void setUserStatus(String userStatus) {
 		this.userStatus = userStatus;
 	}
+
 
 	public String getUserTitle() {
 		return userTitle;
 	}
 
+
 	public void setUserTitle(String userTitle) {
 		this.userTitle = userTitle;
 	}
+
 
 	public String getUserContent() {
 		return userContent;
 	}
 
+
 	public void setUserContent(String userContent) {
 		this.userContent = userContent;
 	}
+
 
 	public int getUserStar() {
 		return userStar;
 	}
 
+
 	public void setUserStar(int userStar) {
 		this.userStar = userStar;
 	}
 
-	public String getUserRegistrationDate() {
-		return userRegistrationDate;
-	}
-
-	public void setUserRegistrationDate(String userRegistrationDate) {
-		this.userRegistrationDate = userRegistrationDate;
-	}
-
-	public String getUserEndDate() {
-		return userEndDate;
-	}
-
-	public void setUserEndDate(String userEndDate) {
-		this.userEndDate = userEndDate;
-	}
 
 	@Override
 	public String toString() {
 		return "UserVO [userNumber=" + userNumber + ", userId=" + userId + ", userPassword=" + userPassword
 				+ ", userName=" + userName + ", userAddress=" + userAddress + ", userPhonenumber=" + userPhonenumber
 				+ ", userAge=" + userAge + ", userGender=" + userGender + ", userStatus=" + userStatus + ", userTitle="
-				+ userTitle + ", userContent=" + userContent + ", userStar=" + userStar + ", userRegistrationDate="
-				+ userRegistrationDate + ", userEndDate=" + userEndDate + "]";
-
+				+ userTitle + ", userContent=" + userContent + ", userStar=" + userStar + "]";
 	}
+
 
 	@Override
 	public int hashCode() {
@@ -147,6 +154,7 @@ public class UserVO {
 		result = prime * result + ((userNumber == null) ? 0 : userNumber.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -165,6 +173,9 @@ public class UserVO {
 		return true;
 	}
 	
+	
+
+
 	
 	
 }
